58e0a8c298a60bb75ece8d859744ee2c382b8001
Andrea Mostosi <andrea.mostosi@zenkay.net>
2013-01-01 18:45:00 +0100

    Responsive theme

---
+++ wp-content/themes/responsive/js/responsive-plugins.js
@@ -0,0 +1,24 @@
+/*!
+ * Responsive JS Plugins v1.1.0
+ */
+// Placeholder
+jQuery(function(){
+    jQuery('input[placeholder], textarea[placeholder]').placeholder();
+});
+// FitVids
+jQuery(document).ready(function(){
+// Target your #container, #wrapper etc.
+    jQuery("#wrapper").fitVids();
+});
+
+// Have a custom video player? We now have a customSelector option where you can add your own specific video vendor selector (mileage may vary depending on vendor and fluidity of player):
+// jQuery("#thing-with-videos").fitVids({ customSelector: "iframe[src^='http://example.com'], iframe[src^='http://example.org']"});
+// Selectors are comma separated, just like CSS
+// Note: This will be the quickest way to add your own custom vendor as well as test your player's compatibility with FitVids.
+
+// Responsive Menu (TinyNav)
+jQuery(".menu").tinyNav({
+    active: 'current_page_item', // Set the "active" class
+	label: '', // String: Sets the <label> text for the <select> (if not set, no label will be added)
+    header: '' // String: Specify text for "header" and show header instead of the active item
+});
