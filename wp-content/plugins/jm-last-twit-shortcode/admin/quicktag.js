921f16f75c09bd827fcfc388779d2336d55c4c03
Andrea Mostosi <andrea.mostosi@zenkay.net>
2013-07-18 10:03:40 +0200

    Plugin for Twitter Stream

---
+++ wp-content/plugins/jm-last-twit-shortcode/admin/quicktag.js
@@ -0,0 +1,4 @@
+//quicktags for plugin JM Last Twit Shortcode
+QTags.addButton( 'eg_jmltsc', 'jmlt', '[jmlt]', '', 'q', 'Last Twit Shortcode', 130 );
+QTags.addButton( 'eg_jmltsc_count', 'jmlt count', '[jmlt count=""]', '', 'q', 'Last Twit Shortcode count', 130 );
+QTags.addButton( 'eg_jmltsc_cache', 'jmlt cache', '[jmlt cache=""]', '', 'q', 'Last Twit Shortcode cache', 130 );
