3cc86c8ffdc02f3d2e4d17964901545bc7b012ed
Andrea Mostosi <andrea.mostosi@zenkay.net>
2015-09-04 20:43:14 +0200

    Added Opeshift inc

--- www/wp-config.php
+++ www/wp-config.php
@@ -116,7 +116,7 @@ define('WPLANG', '');
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
 
 define('FORCE_SSL_ADMIN', false);
 
